3105412abe7ea8e41b3adbe0960266982893107d
anthony2004lp <144872785+anthony2004lp@users.noreply.github.com>
2025-04-12 16:57:27 -0500

    Update imports in main.dart for improved structure

--- app_postsalud/lib/main.dart
+++ app_postsalud/lib/main.dart
@@ -1,6 +1,6 @@
 import 'package:flutter/material.dart';
-import 'package:flutter_application_1/screens/home_screen.dart';
-import 'package:flutter_application_1/screens/login_screen.dart';
+import 'package:app_postsalud/screens/home_screen.dart';
+import 'package:app_postsalud/screens/login_screen.dart';
 
 void main() => runApp(const MyApp());
 
